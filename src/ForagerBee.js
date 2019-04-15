cfebfdf2b690e06966b03e9d5269b279bdbe32e9
Matthew Mata <matthewmata1030@gmail.com>
2019-04-15 10:40:16 -0700

    Finished 6ees^3

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(props) {
+    super(props);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(props) {
+    super(props);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey'
+  }
+
+  forage() {
+    return "I am too old, let me play cards instead";
+  }
+  
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
+  
 };
